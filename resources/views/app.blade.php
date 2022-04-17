92a7d45d32b104c46813f25cc5b5749bb5751294
younesftaich <76970345+younesftaich@users.noreply.github.com>
2022-04-17 08:47:01 +0800

    AA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,16 +10,9 @@
 <link rel="icon" type="image/x-icon" href="https://res.cloudinary.com/luxiptv/image/upload/v1647533208/fastt_1_nrmss1_1_jkvmol.png">
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="2a9863ff-a31c-4bf6-8bed-69e2a6f23472";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="f050b4fb-9438-487e-b94d-8c60b79c8024";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KS321FD4C6"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-KS321FD4C6');
-</script>
 
 
   </head>
